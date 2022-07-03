ebc6f1ac368707819e060e032c3fc6a8f0dbef5e
Mustafa Kursun <mustafa.kursun@futuready.com>
2022-07-04 05:19:51 +0700

    remove unused utility function

--- src/utils/array.utils.js
+++ src/utils/array.utils.js
@@ -1,26 +1,23 @@
 /**
  *
  * @param {Object[]} array
- * @param {Function} keyFunc (arrayPlaceholder) => arrayPlaceholder.mappedElement
- * @returns {Object}
- */
-module.exports.groupArrayOfObjectsByKey = (array, key) => {
-  return array.reduce((counts, { key }) => {
-    counts[key] = (counts[key] || 0) + 1;
-    return counts;
-  }, {});
-};
-
-/**
- *
- * @param {Object[]} array
  * @returns {Object[]}
  */
-module.exports.filterArrayOfObjectsByUniqueObjectProperty = (array) => {
+module.exports.filterArrayOfObjectsByUniqueObjectProperty = (
+  array,
+  propertyKey
+) => {
   if (!Array.isArray(array)) {
     throw new Error("A non-array type passed to the function");
   }
-  return array.filter((value, index, self) => {
-    return self.findIndex((v) => v.id == value.id) === index;
+  if (typeof propertyKey !== "sring") {
+    throw new Errro("Argument propertyKey should be a string");
+  }
+  return array.filter((filterValue, index, self) => {
+    return (
+      self.findIndex(
+        (arrayItem) => arrayItem[propertyKey] == filterValue[propertyKey]
+      ) === index
+    );
   });
 };
