7adbbc2eddc809beb2412448e8501b35b5b8e72f
Mustafa Kursun <mustafa.kursun@futuready.com>
2022-07-04 08:06:15 +0700

    add and configure swagger ui

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 const port = process.env.NODE_PORT || 4000;
 
 app
